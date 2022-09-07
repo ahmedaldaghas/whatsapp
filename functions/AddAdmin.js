895e17c2c14cc16e69d2b45d821306cdbc1b1ece
akaa0 <20195522@stu.uob.edu.bh>
2022-09-07 20:42:55 +0300

    update admin messages

--- functions/AddAdmin.js
+++ functions/AddAdmin.js
@@ -1,23 +1,23 @@
 const { con } = require("./connectionDB");
 
 const addAdmin = function addAdmin(msg, who) {
-    let x = msg.slice(2)+"@c.us";
+    let x = msg.slice(2) + "@c.us";
     return new Promise(async function (resolve, reject) {
         con.query("SELECT * FROM ADMINS WHERE ADMIN=? AND CanAdd=1;", [who], async (err, result) => {
             if (err) return reject(err);
-            if(result.length === 1){
+            if (result.length === 1) {
                 con.query("INSERT INTO ADMINS (ADMIN,WhoAddHim,CanAdd) VALUES (?,?,0);", [x, who], async (err, result) => {
-                if (err) return reject(err);
-                if (result.affectedRows === 1) {
-                    return resolve("DONE");
-                }
-                else return resolve("FAIL");
-            })
+                    if (err) return reject(err);
+                    if (result.affectedRows === 1) {
+                        return resolve("DONE");
+                    }
+                    else return resolve("FAIL");
+                })
             }
-            else{
+            else {
                 return resolve("FAIL");
             }
-            
+
         })
     })
 }
--- functions/DeleteLink.js
+++ functions/DeleteLink.js
@@ -1,36 +1,36 @@
 const { con } = require("./connectionDB");
 
 const deleteLink = function deleteLink(msg, who) {
-            return new Promise(async function (resolve, reject) {
-                con.query("SELECT * FROM ADMINS WHERE ADMIN=? AND CanAdd=1;", [who], async (err, result) => {
-                    if (err) return reject(err);
-                    if(result.length === 1){
-                        msg = msg.split(' ');
-                        if (msg.length !== 2) return resolve("*Sorry*\n\n*But your format is not correct*\n\n*!ITCSxxx SEC*\n\n*make sure of the spacing*");
-                        let x = msg[0].slice(2);
-                        let y = msg[1];
+    return new Promise(async function (resolve, reject) {
+        con.query("SELECT * FROM ADMINS WHERE ADMIN=?;", [who], async (err, result) => {
+            if (err) return reject(err);
+            if (result.length === 1) {
+                msg = msg.split(' ');
+                if (msg.length !== 2) return resolve("*Sorry*\n\n*But your format is not correct*\n\n*!ITCSxxx SEC*\n\n*make sure of the spacing*");
+                let x = msg[0].slice(2);
+                let y = msg[1];
                 con.query("DELETE FROM links WHERE idsec IN ( SELECT l.id FROM sections l, subjects s WHERE s.subject=? AND l.idsc=s.id AND l.sec=? ) limit 1;", [x, y], async (err, result) => {
                     if (err) return reject(err);
                     if (result.affectedRows === 0) {
                         con.query("DELETE FROM links WHERE idsec IN ( SELECT id FROM sections WHERE idsc=(SELECT link FROM subjects WHERE SUBJECT=?) AND sec=? ) limit 1;", [x, y], async (err, result) => {
                             if (err) return reject(err);
                             if (result.affectedRows === 0) {
-                                return resolve("Not Found");
+                                return resolve("This course is not found please double check");
                             }
                             else {
-                                return resolve("DELETED");
+                                return resolve(`${x} has been deleted`);
                             }
                         })
                     }
                     else {
-                        return resolve("DELETED");
+                        return resolve(`${x} has been deleted`);
                     }
                 })
             }
-                else{
-                    return resolve("FAIL")
-                }
-            })
+            else {
+                return resolve("Sorry you are not an admin");
+            }
+        })
     })
 }
 
